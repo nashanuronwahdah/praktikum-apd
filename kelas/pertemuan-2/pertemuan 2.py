fb485f7ca89c9327275b611c1db64b3539958696
nashanuronwahdah <nashanuronwahdah@gmail.com>
2025-09-29 13:08:38 +0800

    recap pertemuan 3

---
+++ kelas/pertemuan-2/pertemuan 2.py
@@ -0,0 +1,20 @@
+
+
+teks = 'saya suka belajar apd'
+
+prodi = ['Informatika', 'Sistem Informasi', 'Teknik Sipil']
+print (prodi[0])
+print(prodi[1])
+print(prodi[2])
+
+
+biodata = {
+    'nama' : 'Nuron',
+    'kelas' : 'A',
+    'usia' : 17,
+}
+
+print (biodata['nama'])
+print (biodata['kelas'])
+print (biodata['usia'])
+
---
+++ kelas/pertemuan-3/pertemuan-3.py
@@ -0,0 +1,116 @@
+# angka = 6
+
+# if angka >=5:
+#     print('angka lebih besar dari 5')
+
+# contoh pertama
+# cuaca = "hujan"
+
+# if cuaca == "hujan":
+#     print('Dirumah aja')
+# else:
+#     print("nongkrong")
+
+# contoh kedua
+# nilai = 70
+
+# if nilai >= 70:
+#     print('lulus')
+# else:
+#     print('tidak lulus')
+
+# nilai = 70
+# status ='lulus' if nilai >= 70 else'Tidak lulus'
+# print(status)
+
+# contoh ketiga
+# cuaca = 'mendung'
+
+# if cuaca == "hujan":
+#     print('dirumah aja')
+# elif cuaca == "mendung":
+#     print('makan mie')
+# else:
+#     print('nongkrong')
+
+# contoh keempat
+
+# usia = int(input('masukkan usia anda: '))
+
+# if usia <= 0:
+#     print('bukan manusia inimah')
+# if usia <= 13:
+#     print('anak anak')
+# elif usia <= 18:
+#     print('remaja')
+# elif usia <= 40:
+#     print('dewasa')
+# else:
+#     print('tua')
+
+# contoh kelima
+# input_nilai = int(input('Masukkan nilai anda: '))
+
+# if input_nilai >= 90:
+#     print('Nilai A')
+# elif input_nilai >= 70:
+#     print('Nilai B')
+# elif input_nilai >= 60:
+#     print('Nilai C')
+# else:
+#     print('Nilai D')
+
+# contoh keenam:
+# a = 4
+# b = 5
+# c = 6
+
+# if a<b:
+#     if a<c:
+#         print('a paling kecil')
+#     else: 
+#         print ('c lebih kecil dari a')
+# elif a<c:
+#     print('c paling besar')
+# else:
+#     print('a paling besar')
+
+# studikasus 1
+# input_tinggi = int(input('Masukkan tinggi badan anda (dalam cm): '))
+
+# if input_tinggi < 145:
+#     print('Lu masih bocil bro!')
+# else:
+#     print('Silahkan masuk tuan!')
+
+# studikasus 2
+
+# input_total = int(input('Masukkan total belanjaan anda: '))
+
+# if input_total >= 100000:
+#     diskon1 = input_total * 20/100
+#     total_akhir = input_total - diskon1
+#     print('Anda dapat diskon 20%, ini total anda ya, Rp' + str(total_akhir))
+# elif input_total >= 50000:
+#     diskon2 = input_total * 10/100
+#     total_akhir2 = input_total - diskon2
+#     print('Anda dapat diskon 10%, ini total anda ya, Rp' + str(total_akhir2))
+# else:
+#     print('Tidak ada diskon, ini total anda Rp' + str(input_total))
+
+
+# nilai = int(input('Masukkan nilai: '))
+# kelas = input('masukkan nilai: ')
+
+# if nilai >= 80 and kelas == 'A' or kelas == 'a':
+#     print('IPK 4')
+# elif nilai >= 80 and kelas == 'B':
+#     print('IPK 3')
+# else:
+#     print('IPK 2')
+
+
+
+
+
+
